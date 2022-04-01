25998be37324e9ba17ca52172d59351e2502ea3f
Rohit Mishra <87615572+shockz09@users.noreply.github.com>
2022-04-01 23:01:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 
 console.log("%cHello World", "color: red");
+console.log("%cHello World", "color: green");
+console.log("%cHello World", "color: orange");
+console.log("%cHello World", "color: yellow");
